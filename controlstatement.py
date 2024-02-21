390c835fc4f4cf849eec446c0811be1a77cfe91b
venkatrj007 <venkatrj007@gmail.com>
2024-02-21 18:20:18 +0530

    my test commit

---
+++ controlstatement.py
@@ -0,0 +1,22 @@
+#wap to check wheather given number is postive or negative and print a message if 
+# n=66
+# if n>0:
+#     print(n,"is positive number")
+
+# o/p:66 is positive
+
+#wap to display hello world if number is greater than 1 and lessthan 5
+  # n=2
+  # if n>1 and n<5:
+  # print("hello world")
+
+# #wap to check whether given number is divisible by 3 or not.if yes print fizz
+#     n=9
+#     if n%3==0:
+#         print("fizz")    
+l=[95,6,"hello",9+5j,88]
+start = 0
+end=len(l)
+while start<end:
+   print(start,end=" ")
+   start += 1
